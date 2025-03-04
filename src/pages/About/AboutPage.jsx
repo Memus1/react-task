90237ae5507fca4adfea565e6ea52550b362889b
Memus1 <mekhrinisoabdupattoyeva@gmail.com>
2025-03-04 22:17:51 +0500

    products div yaratildi

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -9,7 +9,32 @@ function AboutPage() {
   const [producNarx, setProductNarx] = useState("");
   const [productKg, setProductKg] = useState("");
 
-  
+  const AddProduct = () => {
+    const cost = parseFloat(producNarx) * parseFloat(productKg);
+
+    if (miqdor !== null && cost > miqdor) {
+      alert("Pul yetarli emas");
+      return;
+    }
+
+    if (productsName && producNarx && productKg) {
+      setMahsulotlar([
+        ...mahsulotlar,
+        {
+          name: productsName,
+          price: parseFloat(producNarx),
+          kg: parseFloat(productKg),
+        },
+      ]);
+      setMiqdor((prev) =>Number(prev) - cost); 
+
+      
+      setProductName("");
+      setProductNarx("");
+      setProductKg("");
+    }
+   };
+ 
 
   
 
@@ -29,6 +54,45 @@ function AboutPage() {
           Enter
         </button>
         <h1>Jami pul: {miqdor}so'm</h1> 
+      </div>
+
+      <div className="products">
+        <input
+          value={productsName}
+          onChange={(e) => setProductName(e.target.value)}
+          type="text"
+          placeholder="Mahsulot nomi"
+          className="inp"
+        />
+        <input
+          value={producNarx}
+          onChange={(e) => setProductNarx(e.target.value)}
+          type="number"
+          placeholder="Narxi"
+          className="inp"
+        />
+        <input
+          value={productKg}
+          onChange={(e) => setProductKg(e.target.value)}
+          type="number"
+          placeholder="Kg"
+          className="inp"
+        />
+        <button onClick={AddProduct} className="btn">
+          Add
+        </button>
+      </div>
+
+      
+      <div className="p-card">
+        {mahsulotlar.map((mahsulot, index) => (
+          <div key={index} className="product-list">
+            <h2>{mahsulot.name}</h2> 
+            <h3>Narxi: {mahsulot.price} so'm</h3> 
+            <h3>Kg: {mahsulot.kg}</h3> 
+            <p>Jami: {mahsulot.price * mahsulot.kg} so'm</p> 
+            </div>
+        ))}
       </div>
 
       
